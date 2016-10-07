47f6933dd1014e101d44ffb1e622716cb3770b30
mat33470 <mat33470@gmail.com>
2016-10-07 17:11:36 +0200

    perf

--- src/driver.c
+++ src/driver.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 #include "util.h"
-#include "ddot.h"
+#include "cblas.h"
+#include "perf.h"
 
 int main(int argc, char ** argv){
   
@@ -23,27 +24,44 @@ int main(int argc, char ** argv){
   //Affichage de la Matrice
   affiche(m, n, A, lda, stdout);
 
-
   double * X, * Y;
+  m = 50;
 
-  int incx = allouer_vecteur(&X, m);
-  int incy = allouer_vecteur(&Y, m);
+  perf_t begin, end;
 
+  while(m < 1000000){
   //Initialisation des vecteurs
   for (i = 0; i < m; ++i){
     X[i] = i;
     Y[i] = i;
   }
 
+    int incx = allouer_vecteur(&X, m);
+    int incy = allouer_vecteur(&Y, m);
   //Test du Produit Scalaire
   double dot = cblas_ddot(m, X, incx, Y, incy);
 
-  printf ("    X = ");
-  affiche(1, m, X, incx, stdout);
-  printf ("    Y = ");
-  affiche(1, m, Y, incy, stdout);
+    for (i = 0; i < m; ++i){
+      X[i] = i;
+      Y[i] = i;
+    }
 
-  printf("     X * Y = %10.0f\n", dot);
+    perf(&begin);
+    cblas_ddot(m, X, incx, Y, incy);
+    perf(&end);
+
+    printf("perf avec m = %d :\n", m);
+    perf_diff(&begin, &end);
+    printf("   Temps d'execution : "); perf_printmicro(&end);
+    printf("   MFlops/s : %f\n\n", perf_mflops(&end, (long) m));
+
+    desallouer_vecteur(X);
+    desallouer_vecteur(Y);
+
+    m += m/4;
+  }
+  
+
 
   desallouer_matrice(A);
 }
