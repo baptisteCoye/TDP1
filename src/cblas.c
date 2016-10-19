d78994545f69500c35774b38fb35a39c0ed9a4b1
mat33470 <mat33470@gmail.com>
2016-10-19 09:45:23 +0200

    dger

--- src/cblas.c
+++ src/cblas.c
@@ -113,6 +113,17 @@ void cblas_dgemv(const enum CBLAS_ORDER Order,
   }
 }
 
+void cblas_dger(const enum CBLAS_ORDER order, const int M, const int N,
+                const double alpha, const double *X, const int incX,
+                const double *Y, const int incY, double *A, const int lda){
+  assert(order == CblasColMajor);
+
+  for (int i = 0; i < M; i++){
+    for (int j = 0; j < N; j++){
+      A[i+j*lda] += alpha*X[i*incX]*Y[j*incY];
+    }
+  }
+}
 
 void cblas_daxpy(const int N, const double alpha, const double *X,
 		 const int incX, double *Y, const int incY){
